3272b32bb46fe55739c6f977ba8ccd4c2411ea7d
Brandon DeRosier <x@bdero.me>
2020-04-02 04:28:17 -0700

    Communicate between client and server

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,1 +1,16 @@
-console.log("test");
+import {flatbuffers} from "flatbuffers"
+
+import {BlackBox as Buffers} from "../../shared/protos/messages_generated"
+
+const builder = new flatbuffers.Builder()
+Buffers.Message.startMessage(builder)
+const loginMessage = builder.asUint8Array();
+
+const socket = new WebSocket("ws://localhost:8888")
+
+socket.onopen = (event) => {
+    socket.send(loginMessage)
+}
+socket.onmessage = (event) => {
+    console.log(`Message received from server: ${event.data}`)
+}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,32 @@
+import * as http from "http"
 import WebSocket = require("ws")
-import Flags from "./flags"
+import {flatbuffers} from "flatbuffers"
+
 import {BlackBox as Buffers} from "../../shared/protos/messages_generated"
+import Flags from "./flags"
 
-const builder = new flatbuffers.Builder();
+const builder = new flatbuffers.Builder()
 
-const wss = new WebSocket.Server({port: Flags["--port"].value})
-console.log(Flags["--port"].value)
+const httpServer = http.createServer()
+const wsServer = new WebSocket.Server({ noServer: true })
+
+wsServer.on("connection", (socket, request) => {
+    console.log("New connection!")
+    socket.on("message", (data) => {
+        console.log(`Received message from client: ${data}`)
+        socket.send("I'm the server, responding to your message")
+    })
+})
+
+httpServer.on("upgrade", (request, socket, head) => {
+    //TODO(bdero): Do authentication
+
+    console.log("Performing protocol upgrade")
+    wsServer.handleUpgrade(request, socket, head, (socket) => {
+        wsServer.emit("connection", socket, request)
+    })
+})
+
+const port = Flags["--port"].value;
+console.log(`Starting websocket server on port ${port}...`)
+httpServer.listen(port)
