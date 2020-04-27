979df8e63932abca4e49798240539348e431acc9
Brandon DeRosier <x@bdero.me>
2020-04-27 03:46:28 -0700

    Atom placement and basic board rules

--- client/src/dom/gameplay_view.tsx
+++ client/src/dom/gameplay_view.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import {Vector2, GameState, GameBoard} from "../shared/src/messages"
+import { BlackBox } from "../shared/src/protos/messages_generated"
 
 export class GamePlayView extends React.Component<{gameState: GameState}> {
     constructor(props) {
@@ -42,8 +43,10 @@ export class GamePlayView extends React.Component<{gameState: GameState}> {
                 <div className="game-board-container">
                     <GameBoardComponent
                         gameBoard={this.props.gameState.boardB}
-                        isCurrentPlayer={isCurrentPlayer}
-                        isJoined={hasPlayerJoined} />
+                        gameBoardIndex={playerIndex}
+                        seatNumber={this.props.gameState.metadata.seatNumber}
+                        isJoined={hasPlayerJoined}
+                        gameStatus={this.props.gameState.metadata.status} />
                     {inviteLink}
                 </div>
             </div>
@@ -66,11 +69,13 @@ export class GamePlayView extends React.Component<{gameState: GameState}> {
 
 interface GameBoardProps {
     gameBoard: GameBoard,
-    isCurrentPlayer: boolean,
-    isJoined: boolean
+    gameBoardIndex: number,
+    seatNumber: number,
+    isJoined: boolean,
+    gameStatus: BlackBox.GameSessionStatus
 }
 
-class GameBoardComponent extends React.Component<GameBoardProps ,{rayCoords: null | Vector2}> {
+class GameBoardComponent extends React.Component<GameBoardProps, {rayCoords: null | Vector2, localAtoms: Vector2[]}> {
     static readonly CELL_SIZE: number = 100;
     static readonly CELL_BORDER_SIZE: number = 3;
     static readonly BOARD_SIZE: number = 500;
@@ -79,10 +84,17 @@ class GameBoardComponent extends React.Component<GameBoardProps ,{rayCoords: nul
         super(props)
 
         this.state = {
-            rayCoords: null
+            rayCoords: null,
+            localAtoms: []
         }
 
         this.setRayCoords = this.setRayCoords.bind(this)
+        this.isMoveAllowed = this.isMoveAllowed.bind(this)
+        this.localUserIsPlayer = this.localUserIsPlayer.bind(this)
+        this.isLocalPlayerBoard = this.isLocalPlayerBoard.bind(this)
+        this.isAtomSelectionAllowed = this.isAtomSelectionAllowed.bind(this)
+        this.addAtom = this.addAtom.bind(this)
+        this.removeAtom = this.removeAtom.bind(this)
     }
 
     setRayCoords(x?: number, y?: number) {
@@ -93,7 +105,55 @@ class GameBoardComponent extends React.Component<GameBoardProps ,{rayCoords: nul
         this.setState({rayCoords: new Vector2(x, y)})
     }
 
-    getCells() {
+    // Determines if the player is allowed to make moves on this game board
+    isMoveAllowed(): boolean {
+        // If it's the local player's turn, the player is allowed to submit moves against the opponent's game board
+        return (
+            (this.props.seatNumber === 0 && this.props.gameStatus === BlackBox.GameSessionStatus.PlayerATurn && this.props.gameBoardIndex === 1)
+            || (this.props.seatNumber === 1 && this.props.gameStatus === BlackBox.GameSessionStatus.PlayerBTurn && this.props.gameBoardIndex === 0)
+        )
+    }
+
+    // Determines if the local user is a player; false means the user is a spectator
+    localUserIsPlayer(): boolean {
+        return this.props.seatNumber === 0 || this.props.seatNumber === 1
+    }
+
+    // Determines if the current game board is the local player's game board
+    isLocalPlayerBoard(): boolean {
+        return this.props.seatNumber == this.props.gameBoardIndex
+    }
+
+    isAtomSelectionAllowed(): boolean {
+        this.localUserIsPlayer() && this.isLocalPlayerBoard() && this.props.gameStatus === BlackBox.GameSessionStatus.SelectingAtoms
+        this.localUserIsPlayer() && !this.isLocalPlayerBoard() && (this.props.gameStatus === BlackBox.GameSessionStatus.PlayerATurn || this.props.gameStatus === BlackBox.GameSessionStatus.PlayerBTurn)
+        return this.localUserIsPlayer() && (
+            (this.isLocalPlayerBoard() && this.props.gameStatus === BlackBox.GameSessionStatus.SelectingAtoms)
+            || (
+                !this.isLocalPlayerBoard()
+                && (this.props.gameStatus === BlackBox.GameSessionStatus.PlayerATurn || this.props.gameStatus === BlackBox.GameSessionStatus.PlayerBTurn)
+            )
+        )
+    }
+
+    addAtom(x: number, y: number) {
+        if (this.state.localAtoms.length >= 4) return
+        this.setState(prevState => {
+            const newAtoms = prevState.localAtoms.filter(a => !(a.x === x && a.y === y))
+            newAtoms.push(new Vector2(x, y))
+            return {localAtoms: newAtoms}
+        })
+    }
+
+    removeAtom(x: number, y: number) {
+        this.setState(prevState => {
+            const newAtoms = prevState.localAtoms.filter(a => !(a.x === x && a.y === y))
+        })
+    }
+
+    getCells(): JSX.Element {
+        const isMoveAllowed = this.isMoveAllowed()
+        const isAtomSelectionAllowed = this.isAtomSelectionAllowed()
         const cells: JSX.Element[] = []
         for (let y = 0; y < 10; y++) {
             for (let x = 0; x < 10; x++) {
@@ -103,11 +163,16 @@ class GameBoardComponent extends React.Component<GameBoardProps ,{rayCoords: nul
                 if (cellType === 2) continue  // Don't render corners
 
                 let extraProps = {}
-                if (cellType === 1) {
+                if (cellType === 1 && isMoveAllowed) {
                     extraProps = {
                         onMouseEnter: () => {this.setRayCoords(x, y)},
                         //onMouseOver: () => {this.setRayCoords(x, y)},
                         onMouseLeave: () => {this.setRayCoords()},
+                    }
+                }
+                if (cellType === 0 && isAtomSelectionAllowed) {
+                    extraProps = {
+                        onMouseUp: () => this.addAtom(x - 1, y - 1)
                     }
                 }
                 cells.push(
@@ -130,7 +195,7 @@ class GameBoardComponent extends React.Component<GameBoardProps ,{rayCoords: nul
         )
     }
 
-    getRay() {
+    getRay(): JSX.Element {
         if (this.state.rayCoords === null) return null
 
         const xdir = this.state.rayCoords.x === 0 ? 1 : this.state.rayCoords.x === 9 ? -1 : 0
@@ -150,12 +215,31 @@ class GameBoardComponent extends React.Component<GameBoardProps ,{rayCoords: nul
         />
     }
 
+    getAtoms(): JSX.Element {
+        const atoms = this.state.localAtoms.map(a => {
+            return (
+                <circle
+                    key={`atom_x${a.x}y${a.y}`}
+                    className="atom-circle"
+                    cx={GameBoardComponent.CELL_SIZE*1.5 + a.x*GameBoardComponent.CELL_SIZE}
+                    cy={GameBoardComponent.CELL_SIZE*1.5 + a.y*GameBoardComponent.CELL_SIZE}
+                    r="30"
+                />
+            )
+        })
+        return <g>{atoms}</g>
+    }
+
     render() {
         return (
-            <svg className="game-board" viewBox={`0 0 ${GameBoardComponent.BOARD_SIZE} ${GameBoardComponent.BOARD_SIZE}`} preserveAspectRatio="xMidYMid meet">
+            <svg
+                className="game-board"
+                viewBox={`0 0 ${GameBoardComponent.BOARD_SIZE} ${GameBoardComponent.BOARD_SIZE}`}
+                preserveAspectRatio="xMidYMid meet">
                 <g transform="scale(0.5, 0.5)">
                     {this.getCells()}
                     {this.getRay()}
+                    {this.getAtoms()}
                 </g>
             </svg>
         )
