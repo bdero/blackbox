9092a280c2fce0c4d4c3d53acfce53a88b384f6b
Brandon DeRosier <x@bdero.me>
2020-05-09 05:17:04 -0700

    Add ray solver and path preview

--- client/src/dom/gameplay_view.tsx
+++ client/src/dom/gameplay_view.tsx
@@ -309,6 +309,7 @@ class GameBoardComponent extends React.Component<GameBoardProps, GameBoardState>
                 cells.push(
                     <rect
                         key={`cell_x${x}y${y}`}
+                        rx="8"
                         x={x*GameBoardComponent.CELL_SIZE + GameBoardComponent.CELL_BORDER_SIZE}
                         y={y*GameBoardComponent.CELL_SIZE + GameBoardComponent.CELL_BORDER_SIZE}
                         width={GameBoardComponent.CELL_SIZE - GameBoardComponent.CELL_BORDER_SIZE*2}
@@ -328,6 +329,36 @@ class GameBoardComponent extends React.Component<GameBoardProps, GameBoardState>
 
     getRay(): JSX.Element {
         if (this.state.rayCoords === null) return null
+
+        virtualBoard.setAtoms(...this.state.localAtoms.map(a => Vector2.add(a.position, new Vector2(1, 1))))
+        const pathPoints = virtualBoard.castRay(this.state.rayCoords)
+        const pathString = pathPoints.map((s, i) => (
+            `${i === 0 ? `M` : `L`} ${s.x*GameBoardComponent.CELL_SIZE + GameBoardComponent.CELL_SIZE/2} ${s.y*GameBoardComponent.CELL_SIZE + GameBoardComponent.CELL_SIZE/2}`)
+        ).join(" ")
+        console.log(pathString)
+
+        const first = pathPoints[0]
+        const last = pathPoints.length > 1 ? pathPoints[pathPoints.length - 1] : null
+        return <g>
+            <path
+                className="ray-path"
+                d={pathString}
+            />
+            <circle
+                className="ray-circle"
+                r="18"
+                cx={first.x*GameBoardComponent.CELL_SIZE + GameBoardComponent.CELL_SIZE/2}
+                cy={first.y*GameBoardComponent.CELL_SIZE + GameBoardComponent.CELL_SIZE/2}
+            />
+            {last !== null &&
+                <circle
+                    className="ray-circle"
+                    r="18"
+                    cx={last.x*GameBoardComponent.CELL_SIZE + GameBoardComponent.CELL_SIZE/2}
+                    cy={last.y*GameBoardComponent.CELL_SIZE + GameBoardComponent.CELL_SIZE/2}
+                />
+            }
+        </g>
 
         const xdir = this.state.rayCoords.x === 0 ? 1 : this.state.rayCoords.x === 9 ? -1 : 0
         const ydir = this.state.rayCoords.y === 0 ? 1 : this.state.rayCoords.y === 9 ? -1 : 0
@@ -414,6 +445,90 @@ class GameBoardComponent extends React.Component<GameBoardProps, GameBoardState>
         )
     }
 }
+
+class VirtualBoard {
+    board: Array<Array<boolean>>
+    atoms: Vector2[]
+
+    constructor() {
+        this.board = []
+        for (let i = 0; i < 10; i++) {
+            this.board.push([])
+            for (let j = 0; j < 10; j++) {
+                this.board[i].push(false)
+            }
+        }
+        this.atoms = []
+    }
+
+    clearAtoms() {
+        this.atoms.forEach(a => this.board[a.x][a.y] = false)
+        this.atoms = []
+    }
+
+    setAtoms(...atoms: Vector2[]) {
+        this.clearAtoms()
+        this.atoms.push(...atoms)
+        this.atoms.forEach(a => this.board[a.x][a.y] = true)
+    }
+
+    static isSide(cell: Vector2): boolean {
+        return cell.x <= 0 || cell.x >= 9 || cell.y <= 0 || cell.y >= 9
+    }
+
+    isHit(cell: Vector2): boolean {
+        return !VirtualBoard.isSide(cell) && this.board[cell.x][cell.y]
+    }
+
+    // 10x10 board origin
+    castRay(origin: Vector2): Vector2[] | null {
+        let dir = new Vector2(
+            origin.x === 0 ? 1 : origin.x === 9 ? -1 : 0,
+            origin.y === 0 ? 1 : origin.y === 9 ? -1 : 0)
+        if (dir.x === 0 && dir.y === 0) return null
+        const resultPath: Vector2[] = [Vector2.clone(origin)]
+
+        const getForwardCell = () => Vector2.add(currentCell, dir)
+        const getForwardLeftCell = () => Vector2.clone(dir).rotateLeft().add(dir).add(currentCell)
+        const getForwardRightCell = () => Vector2.clone(dir).rotateRight().add(dir).add(currentCell)
+
+        let currentCell = Vector2.clone(origin)
+        while (!VirtualBoard.isSide(currentCell) || (resultPath.length === 1 && currentCell.equals(resultPath[0]))) {
+            if (this.isHit(getForwardCell())) {
+                currentCell.add(dir)
+                break
+            }
+            const forwardLeftIsHit = this.isHit(getForwardLeftCell())
+            const forwardRightIsHit = this.isHit(getForwardRightCell())
+            if (VirtualBoard.isSide(currentCell) && (forwardLeftIsHit || forwardRightIsHit)) {
+                break
+            }
+            if (forwardLeftIsHit && forwardRightIsHit) {
+                resultPath.push(Vector2.clone(currentCell))
+                dir.rotateRight().rotateRight()
+                currentCell.add(dir)
+                continue
+            }
+            if (forwardLeftIsHit) {
+                resultPath.push(Vector2.clone(currentCell))
+                dir.rotateRight()
+                currentCell.add(dir)
+                continue
+            }
+            if (forwardRightIsHit) {
+                resultPath.push(Vector2.clone(currentCell))
+                dir.rotateLeft()
+                currentCell.add(dir)
+                continue
+            }
+            currentCell.add(dir)
+        }
+
+        resultPath.push(currentCell)
+        return resultPath
+    }
+}
+const virtualBoard = new VirtualBoard()
 
 class Atom extends React.Component<{isStatic: boolean, instance: InstanceState, x: number, y: number}, {visualX: number, visualY: number}> {
     unmounting = false
